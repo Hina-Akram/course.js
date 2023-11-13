14fe34ab0da18c98dec01a183c719ca196f7d462
Hina Akram <67531786+Hina-Akram@users.noreply.github.com>
2023-11-13 17:14:27 +0000

    variables

---
+++ basics_01/variables.js
@@ -0,0 +1,12 @@
+const reg_Id="2020-ag-5559"
+let student_Name="Hina Akram";
+var department_Name="computer science";
+let student_Email;
+
+student_Name= "zohaib"
+department_Name="ICS"
+/*
+not prefer to use variables because of block scope and functional sope issue
+*/
+console.table([reg_Id, student_Name, department_Name, student_Email]);
+
