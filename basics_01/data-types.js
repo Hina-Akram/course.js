778492cd9e466126fab0a1fe7ef498ddbb15ffdf
Hina Akram <67531786+Hina-Akram@users.noreply.github.com>
2023-11-15 06:30:05 +0000

    datatypes

---
+++ basics_01/data-types.js
@@ -0,0 +1,18 @@
+//integer number 
+//bigInt
+//string ""
+//undefined
+//object
+//null standalone
+// boolean true/false
+
+let age=18
+let name="hina"
+let LoogedIn=false
+let subject=null;
+let state;
+
+console.log(typeof age);
+console.log(typeof LoogedIn);
+console.log(typeof subject);//output is object
+console.log(typeof state);//output is undefined
