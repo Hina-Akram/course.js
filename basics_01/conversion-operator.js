db19d4e3547b12f421f12db1c8cb6f689a28ab7b
Hina Akram <67531786+Hina-Akram@users.noreply.github.com>
2023-11-15 07:02:45 +0000

    conversion of data tpe

---
+++ basics_01/conversion-operator.js
@@ -0,0 +1,14 @@
+let number = 33
+let StringNumber = String(number);
+console.log(StringNumber);
+console.log(typeof StringNumber);
+
+
+let LoggedIn = 1
+let BooleanLoggedIN= Boolean(LoggedIn)
+
+console.log(LoggedIn)
+console.log(typeof LoggedIn)
+
+console.log(BooleanLoggedIN)
+console.log(typeof BooleanLoggedIN)
